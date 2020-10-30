08b4b9e14dab0e172d4c28463622e13fbd3e18da
JulianeOlivCosta <73131651+JulianeOlivCosta@users.noreply.github.com>
2020-10-30 18:03:09 -0300

    add plug in no gradle. created buttons and colors. method to change the color.

--- app/src/main/java/com/wcc/colormyviews/MainActivity.kt
+++ app/src/main/java/com/wcc/colormyviews/MainActivity.kt
@@ -2,10 +2,29 @@ package com.wcc.colormyviews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.* // plug in no build gradle para acessar o botao direto pelo ID
 
 class MainActivity : AppCompatActivity() {
+
+    var currentColor = R.color.gray
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setColorButtonAction() //chama método que irá trocar a cor do botao ao clicar
+    }
+
+    private fun setColorButtonAction() {
+        redButton.setOnClickListener{
+            currentColor = R.color.red
+        }
+        yellowButton.setOnClickListener{
+            currentColor = R.color.yellow
+        }
+        greenButton.setOnClickListener{
+            currentColor = R.color.green
+        }
     }
 }
