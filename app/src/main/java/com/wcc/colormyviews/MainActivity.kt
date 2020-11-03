a604b0c095e92ac107715f298064ddc8de3bbf9b
JulianeOlivCosta <73131651+JulianeOlivCosta@users.noreply.github.com>
2020-11-03 19:17:41 -0300

    add caixas faltantes

--- app/src/main/java/com/wcc/colormyviews/MainActivity.kt
+++ app/src/main/java/com/wcc/colormyviews/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.wcc.colormyviews
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.* // plug in no build gradle para acessar o botao direto pelo ID
@@ -7,34 +8,55 @@ import kotlinx.android.synthetic.main.activity_main.* // plug in no build gradle
 class MainActivity : AppCompatActivity() {
 
     var currentColor = R.color.gray
+    var boxOneColor = R.color.gray
+
+    //fazer a mesma ação com as outras caixas
+    var boxTwoColor = R.color.gray
+    var boxThreeColor = R.color.gray
+    var boxFourColor = R.color.gray
+    var boxFiveColor = R.color.gray
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val sharedPreferences = this.getSharedPreferences("colors", Context.MODE_PRIVATE)
+
+        val color = sharedPreferences.getInt("color_box_one", R.color.gray)
+        box_one_test.setBackgroundResource(color)
+
+        val colorTwo = sharedPreferences.getInt("color_box_two", R.color.gray)
+        box_two_test.setBackgroundResource(colorTwo)
+
+        val colorThree = sharedPreferences.getInt("color_box_three", R.color.gray)
+        box_two_test.setBackgroundResource(colorThree)
+
+        val colorFour = sharedPreferences.getInt("color_box_four", R.color.gray)
+        box_two_test.setBackgroundResource(colorFour)
+
+        val colorFive = sharedPreferences.getInt("color_box_five", R.color.gray)
+        box_two_test.setBackgroundResource(colorFive)
+
         setColorButtonAction()
         setBoxColor()//chama método que irá trocar a cor do botao ao clicar
     }
 
-    private fun setBoxColor() {
-        box_one_test.setOnClickListener{
-            it.setBackgroundResource(currentColor)
-        }
-        box_two_test.setOnClickListener{
-            it.setBackgroundResource(currentColor)
-        }
-        box_three_test.setOnClickListener{
-            it.setBackgroundResource(currentColor)
-        }
-        box_four_test.setOnClickListener{
-            it.setBackgroundResource(currentColor)
-        }
-        box_five_test.setOnClickListener{
-            it.setBackgroundResource(currentColor)
-        }
-    }
+    override fun onStop() {
+        super.onStop()
+        val sharedPreferences = this.getSharedPreferences("colors", Context.MODE_PRIVATE)
+        val sharedPreferencesEditor = sharedPreferences.edit()
 
+        sharedPreferencesEditor.putInt("color_box_one", boxOneColor )
+
+        sharedPreferencesEditor.putInt("color_box_two", boxTwoColor)
+        sharedPreferencesEditor.putInt("color_box_three", boxThreeColor)
+        sharedPreferencesEditor.putInt("color_box_four", boxFourColor)
+        sharedPreferencesEditor.putInt("color_box_five", boxFiveColor)
+
+        sharedPreferencesEditor.apply()
+
+    }
     private fun setColorButtonAction() {
         redButton.setOnClickListener{
             currentColor = R.color.red
@@ -44,6 +66,30 @@ class MainActivity : AppCompatActivity() {
         }
         greenButton.setOnClickListener{
             currentColor = R.color.green
+        }
+    }
+    private fun setBoxColor() {
+        box_one_test.setOnClickListener{
+            it.setBackgroundResource(currentColor)
+            boxOneColor = currentColor
+
+        }
+        box_two_test.setOnClickListener{
+            it.setBackgroundResource(currentColor)
+            boxTwoColor = currentColor
+        }
+        box_three_test.setOnClickListener{
+            it.setBackgroundResource(currentColor)
+            boxThreeColor = currentColor
+        }
+        box_four_test.setOnClickListener{
+            it.setBackgroundResource(currentColor)
+            boxFourColor = currentColor
+
+        }
+        box_five_test.setOnClickListener{
+            it.setBackgroundResource(currentColor)
+            boxFiveColor = currentColor
         }
     }
 }
